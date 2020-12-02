942e79654d7361b23ea97c663b1cfdda37e95dd9
Billy Martin <Billymartin910@gmail.com>
2020-12-02 02:44:46 +0000

    Visual Studio code crash fix

--- class8/provider.tf
+++ class8/provider.tf
@@ -1,13 +1,6 @@
 provider "aws" {
   region = var.region
   version = "3.18.0"
-<<<<<<< HEAD
-}
-
-terraform {
-  required_version = "0.12.19"
-=======
->>>>>>> 8dc242ca54053fc7e2eb7ca163248e0a93dcef16
 }
 
 terraform {
